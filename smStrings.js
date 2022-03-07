0f82e320d87b6021d27b2a42ff6d2a87e2c5e86a
sum182 <sum182@gmail.com>
2022-03-07 15:21:11 -0300

    Add

---
+++ smStrings.js
@@ -0,0 +1,13 @@
+const strColorConsoleReset    = '\x1b[0m';
+const strColorConsoleYellow   = '\x1b[33m';
+const strColorConsoleColorRed = '\x1b[41m';
+
+
+
+
+
+module.exports = {
+  strColorConsoleReset,   
+  strColorConsoleYellow,  
+  strColorConsoleColorRed
+}
