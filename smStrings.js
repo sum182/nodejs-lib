7b37a2b65f198aec643aab581c6661ed9af21a7c
sum182 <sum182@gmail.com>
2022-03-07 15:39:10 -0300

    novas funcoes

--- smStrings.js
+++ smStrings.js
@@ -3,11 +3,19 @@ const strColorConsoleYellow   = '\x1b[33m';
 const strColorConsoleColorRed = '\x1b[41m';
 
 
-
+function logConsoleHighlighted(str){
+  /*
+    função para exibir logs destacado
+  */
+  console.log(strColorConsoleYellow,str);  
+  console.log(strColorConsoleReset,'');  
+}
 
 
 module.exports = {
   strColorConsoleReset,   
   strColorConsoleYellow,  
-  strColorConsoleColorRed
+  strColorConsoleColorRed,
+
+  logConsoleHighlighted
 }
